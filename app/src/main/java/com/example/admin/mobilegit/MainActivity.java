b9b9b139904d21eba7f6d6bed6995d2298e3db27
Svitlana Ambros <svitlana.ambros@gmail.com>
2017-09-26 13:18:15 +0300

    add view flipper and error screen

--- app/src/main/java/com/example/admin/mobilegit/MainActivity.java
+++ app/src/main/java/com/example/admin/mobilegit/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.admin.mobilegit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.et_project_name)
+    EditText projectNameEditText;
+
+    @BindView(R.id.view_flipper)
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setListenerForEditText();
+        viewFlipper.showNext();
+    }
+
+    @Override
+    public void onClick(View v) {
+        projectNameEditText.setText("");
+    }
+
+    public void setListenerForEditText(){
+        projectNameEditText.setOnClickListener(this);
+
+        projectNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count > 3){
+                    Toast.makeText(getBaseContext(), "dfd", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
